d747bbcfb2adceba5f1ba1f54dccaa056b831ace
Kurtukov Pavel <pavel@kurtukov.ru>
2024-04-22 23:48:49 +0700

    Initial commit

---
+++ src/main/java/ru/kurtukov/jdbcdao/JdbcDaoApplication.java
@@ -0,0 +1,13 @@
+package ru.kurtukov.jdbcdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcDaoApplication.class, args);
+	}
+
+}
